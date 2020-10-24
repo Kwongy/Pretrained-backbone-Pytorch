1c97bbc0090bae377b10fafca94d134f514f0a3b
Kwongy <760030764@qq.com>
2020-10-24 10:58:58 +0800

    resnet

---
+++ Backbone/resnet.py
@@ -0,0 +1,237 @@
+# _*_ coding: utf-8 _*_
+"""
+    Author: Kwong
+    Create time: 2020/10/24 9:42 
+"""
+
+import torch.nn as nn
+import functools
+import torch
+import torch.utils.model_zoo as model_zoo
+import torch.nn.functional as F
+
+from torchvision.models.resnet import ResNet
+from torchvision.models.resnet import BasicBlock
+from torchvision.models.resnet import Bottleneck
+from pretrainedmodels.models.torchvision_models import pretrained_settings
+
+
+class ResNetEncoder(ResNet):
+    def __init__(self, out_channels, depth=4, **kwargs):
+        super().__init__(**kwargs)
+        self._depth = depth
+        self._out_channels = out_channels
+        self._in_channels = 3
+        del self.fc
+        del self.avgpool
+
+    def get_stages(self):
+        return [
+            # nn.Identity(),
+            nn.Sequential(self.conv1, self.bn1, self.relu, self.maxpool, self.layer1),
+            self.layer2,
+            self.layer3,
+            self.layer4,
+        ]
+
+    def forward(self, x):
+        stages = self.get_stages()
+
+        features = []
+        for i in range(self._depth):
+            x = stages[i](x)
+            features.append(x)
+        return features
+
+
+resnet_encoders = {
+    "resnet18": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet18"],
+        "params": {
+            "out_channels": (3, 64, 64, 128, 256, 512),
+            "block": BasicBlock,
+            "layers": [2, 2, 2, 2],
+        },
+    },
+    "resnet34": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet34"],
+        "params": {
+            "out_channels": (3, 64, 64, 128, 256, 512),
+            "block": BasicBlock,
+            "layers": [3, 4, 6, 3],
+        },
+    },
+    "resnet50": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet50"],
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 6, 3],
+        },
+    },
+    "resnet101": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet101"],
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 23, 3],
+        },
+    },
+    "resnet152": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": pretrained_settings["resnet152"],
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 8, 36, 3],
+        },
+    },
+    "resnext50_32x4d": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": {
+            "imagenet": {
+                "url": "https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            }
+        },
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 6, 3],
+            "groups": 32,
+            "width_per_group": 4,
+        },
+    },
+    "resnext101_32x8d": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": {
+            "imagenet": {
+                "url": "https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            },
+            "instagram": {
+                "url": "https://download.pytorch.org/models/ig_resnext101_32x8-c38310e5.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            },
+        },
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 23, 3],
+            "groups": 32,
+            "width_per_group": 8,
+        },
+    },
+    "resnext101_32x16d": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": {
+            "instagram": {
+                "url": "https://download.pytorch.org/models/ig_resnext101_32x16-c6f796b0.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            }
+        },
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 23, 3],
+            "groups": 32,
+            "width_per_group": 16,
+        },
+    },
+    "resnext101_32x32d": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": {
+            "instagram": {
+                "url": "https://download.pytorch.org/models/ig_resnext101_32x32-e4b90b00.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            }
+        },
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 23, 3],
+            "groups": 32,
+            "width_per_group": 32,
+        },
+    },
+    "resnext101_32x48d": {
+        "encoder": ResNetEncoder,
+        "pretrained_settings": {
+            "instagram": {
+                "url": "https://download.pytorch.org/models/ig_resnext101_32x48-3e41cc8a.pth",
+                "input_space": "RGB",
+                "input_size": [3, 224, 224],
+                "input_range": [0, 1],
+                "mean": [0.485, 0.456, 0.406],
+                "std": [0.229, 0.224, 0.225],
+                "num_classes": 1000,
+            }
+        },
+        "params": {
+            "out_channels": (3, 64, 256, 512, 1024, 2048),
+            "block": Bottleneck,
+            "layers": [3, 4, 23, 3],
+            "groups": 32,
+            "width_per_group": 48,
+        },
+    },
+}
+
+
+def get_encoder(name, weights="imagenet", depth=4):
+    Encoder = resnet_encoders[name]["encoder"]
+    params = resnet_encoders[name]["params"]
+    params.update(depth = depth)
+    encoder = Encoder(**params)
+    if weights is not None:
+        settings = resnet_encoders[name]["pretrained_settings"][weights]
+        pretrained_dict = model_zoo.load_url(settings["url"])
+        model_dict = encoder.state_dict()
+        # 1. filter out unnecessary keys
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        # 2. overwrite entries in the existing state dict
+        model_dict.update(pretrained_dict)
+        encoder.load_state_dict(model_dict)
+    return encoder
+
+
+def main():
+    model = get_encoder("resnext50_32x4d")
+    image = torch.rand([2, 3, 256, 256])
+    result = model(image)
+    # print(result)
+    for i in result:
+        print(i.shape)
+
+
+if __name__ == "__main__":
+    main()
